37df110a95561cf6a7731ad5de90bc4992164e8e
Mavliut Fozliev <mavliut.fozliev@gmail.com>
2024-11-01 11:35:55 +0300

    Initial commit

---
+++ apps/myorg-e2e/jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  displayName: 'myorg-e2e',
+  preset: '../../jest.preset.js',
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/myorg-e2e',
+};
